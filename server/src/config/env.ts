ce2278b6d711f066c8ae484696efb5f605633982
OmkarM9090 <omkarmahadik180@gmail.com>
2026-06-21 12:36:59 +0530

    initial setup is work in progress

---
+++ server/src/config/env.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const requiredEnvVars = [
+    "PORT",
+    "DATABASe_URL"
+] as const;
+
+for(const envVar of requiredEnvVars)
+{
+    if(!process.env[envVar])
+    {
+        throw new Error(
+            `missing required environment varables: ${envVar}`
+        );
+    }
+}
+
+export const env = {
+   PORT: Number(process.env.PORT),
+  DATABASE_URL: process.env.DATABASE_URL!, 
+}
